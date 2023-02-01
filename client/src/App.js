3a6f7db8b8d79df40e8c6d667e69e51f1e5e937b
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2023-02-01 02:08:21 -0500

    fixed some stuff hopefully

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [data, setData] = React.useState(null);
+let [data, setData] = React.useState(null);
 
   React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
-      .then((data) = setData(data.message));
-
+      .then((data) => setData(data.message));
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> {!data ? "Loading..." : data}aaa</p>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {data}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
